2b5b520dd1f4d3f72eeb873c5fd1213e8495fbfc
Дмитрий Витютнев <dimason99@gmail.com>
2020-09-21 21:40:16 +0600

    Day 21. Localisation Really short task

---
+++ Day21_Localization/app/src/main/java/ru/dmitryvitutnev/day21_localization/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.dmitryvitutnev.day21_localization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
